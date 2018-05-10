ec6ea1085fbfd5890d9ec5d9458a6457d830243e
Brian Beyer <brian@redcanary.com>
2018-05-10 14:10:12 -0600

    pull in attack stix

--- generate_atomic_docs.rb
+++ generate_atomic_docs.rb
@@ -2,15 +2,33 @@
 require 'yaml'
 require 'ostruct'
 require 'erb'
+require 'open-uri'
+require 'json'
+
+def attack_technique_library
+  @attack_json ||= begin
+    local_attack_json_to_try = "#{File.dirname(__FILE__)}/enterprise-attack.json"
+    if File.exists? local_attack_json_to_try
+      JSON.parse File.read(local_attack_json_to_try)
+    else
+      JSON.parse open('https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json').read
+    end
+  end
+end
+
+def attack_technique_info(technique_id)
+  attack_technique_library.fetch("objects").find do |item| 
+    item.fetch('external_references', []).find do |references|
+      references['source_name'] == 'mitre-attack' && references['external_id'] == technique_id.upcase
+    end
+  end
+end
 
 def generate_docs!(path)
   atomic_yaml = YAML.load(File.read path)
 
-  technique = {
-    # TODO GET FROM MITRE
-    'identifier' => "T1234",
-    'name' => "Create Account",
-  }
+  technique = attack_technique_info(atomic_yaml.fetch('attack_technique'))
+  technique['identifier'] = atomic_yaml.fetch('attack_technique').upcase
 
   template = ERB.new File.read("#{File.dirname(__FILE__)}/atomics/atomic_doc_template.md.erb"), nil, "-"
   generated_doc = template.result(binding)
