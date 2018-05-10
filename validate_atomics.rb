d26aa7dd3468320570bb346bb8aacae4ec89dd09
Brian Beyer <brian@redcanary.com>
2018-05-10 11:31:35 -0600

    bring all existing atomics in line with spec

--- validate_atomics.rb
+++ validate_atomics.rb
@@ -47,63 +47,32 @@ def validate_is_atomic!(path)
       raise("`atomic_tests[#{i}].input_arguments[#{iai}].type` element must be a string") unless arg['type'].is_a?(String)
       raise("`atomic_tests[#{i}].input_arguments[#{iai}].type` element must be lowercased and underscored (was #{arg['type']})") unless arg['type'] =~ /[a-z_]+/
 
-      raise("`atomic_tests[#{i}].input_arguments[#{iai}].default` element is required") unless arg.has_key?('default')
+      # TODO: determine if we think default values are required for EVERY input argument
+      # raise("`atomic_tests[#{i}].input_arguments[#{iai}].default` element is required") unless arg.has_key?('default')
       # raise("`atomic_tests[#{i}].input_arguments[#{iai}].default` element must be a string (was a #{arg['default'].class.name})") unless arg['default'].is_a?(String)
     end
 
-    raise("`atomic_tests[#{i}].executors` element is required") unless atomic.has_key?('executors')
-    raise("`atomic_tests[#{i}].executors` element must be an Array") unless atomic['executors'].is_a?(Array)
-    raise("`atomic_tests[#{i}].executors` element is empty - you have no way to execute this test") unless atomic['executors'].count > 0
+    raise("`atomic_tests[#{i}].executor` element is required") unless atomic.has_key?('executor')
+    executor = atomic['executor']
+    raise("`atomic_tests[#{i}].executor.name` element is required") unless executor.has_key?('name')
+    raise("`atomic_tests[#{i}].executor.name` element must be a string") unless executor['name'].is_a?(String)
+    raise("`atomic_tests[#{i}].executor.name` element must be lowercased and underscored (was #{executor['name']})") unless executor['name'] =~ /[a-z_]+/
 
-    atomic['executors'].each_with_index do |executor, ei|
-      raise("`atomic_tests[#{i}].executors[#{ei}].name` element is required") unless executor.has_key?('name')
-      raise("`atomic_tests[#{i}].executors[#{ei}].name` element must be a string") unless executor['name'].is_a?(String)
-      raise("`atomic_tests[#{i}].executors[#{ei}].name` element must be lowercased and underscored (was #{executor['name']})") unless executor['name'] =~ /[a-z_]+/
+    valid_executor_types = ['command_prompt', 'sh', 'bash', 'powershell', 'manual']
+    case executor['name']
+      when 'manual'
+        raise("`atomic_tests[#{i}].executor.steps` element is required") unless executor.has_key?('steps')
+        raise("`atomic_tests[#{i}].executor.steps` element must be a string") unless executor['steps'].is_a?(String)
 
-      valid_executor_types = ['command_prompt', 'sh', 'bash', 'powershell', 'manual']
-      case executor['name']
-        when 'manual'
-          raise("`atomic_tests[#{i}].executors[#{ei}].steps` element is required") unless executor.has_key?('command')
-          raise("`atomic_tests[#{i}].executors[#{ei}].steps` element must be a string") unless executor['command'].is_a?(String)
+      when 'command_prompt', 'sh', 'bash', 'powershell'
+        raise("`atomic_tests[#{i}].executor.command` element is required") unless executor.has_key?('command')
+        raise("`atomic_tests[#{i}].executor.command` element must be a string") unless executor['command'].is_a?(String)
 
-        when 'command_prompt', 'sh', 'bash', 'powershell'
-          raise("`atomic_tests[#{i}].executors[#{ei}].command` element is required") unless executor.has_key?('command')
-          raise("`atomic_tests[#{i}].executors[#{ei}].command` element must be a string") unless executor['command'].is_a?(String)
-
-        else
-          raise("`atomic_tests[#{i}].executors[#{ei}].name` '#{executor['name']}' must be one of #{valid_executor_types.join(', ')}")
-      end
+      else
+        raise("`atomic_tests[#{i}].executor.name` '#{executor['name']}' must be one of #{valid_executor_types.join(', ')}")
     end
   end
 end
-
-
-#
-# atomic_tests:
-# - name: SourceRecorder via Windows command prompt
-#   description: |
-#     Create a file called test.wma, with the duration of 30 seconds
-#
-#   supported_platforms:
-#     - windows
-#
-#   input_arguments:
-#     output_file:
-#       description: xxxxx
-#       type: Path
-#       default: test.wma
-#
-#     duration_hms:
-#       description: xxxxx
-#       type: Path
-#       default: 0000:00:30
-#
-#   executors:
-#   - name: command_prompt
-#     command: |
-#       SoundRecorder /FILE #{output_file} /DURATION #{duration_hms}
-#
-
 
 oks = []
 fails = []
