1fb0d98e6a5b4adfec7ce4b5d637e81a4fe6351e
Brian Beyer <brian@redcanary.com>
2018-06-08 12:26:37 -0500

    remove the old Linux, Mac, Windows directories now that payloads are inside `atomics`

--- Mac/Payloads/hello.c
+++
@@ -1,9 +0,0 @@
-#import <stdio.h>
-#import <unistd.h>
-int main()
-{
-    printf("Hello\n");
-    sleep(60);
-    printf("Don't run random binaries!\n");
-    return 0;
-}
--- Windows/Payloads/Collection.bat
+++
@@ -1,4 +0,0 @@
-SoundRecorder /FILE test.wma /DURATION 0000:00:30
-echo Get-Process > process-information.txt
-powershell
-Get-Clipboard | iex
