46c56eee4f934dd1e4756e7c79ead97774ac5c6c
caseysmithrc <30840394+caseysmithrc@users.noreply.github.com>
2018-09-04 09:28:28 -0600

    Clean Up Style and README

--- execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam.ps1
+++ execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam.ps1
@@ -3,7 +3,6 @@
 This script will iterate over the Atomic Red Team yaml files, create objects for each test.
 The aim is to allow defenders to excercise MITRE ATT&CK Techniques to test defenses.
 
-
 Function: Invoke-AtomicRedTeam
 Author: Casey Smith @subTee
 License:  http://opensource.org/licenses/MIT
@@ -18,9 +17,12 @@ Create Atomic Tests from yaml files described in Atomic Red Team. https://github
 Convert Single Yaml File to Technique Object
 $T1117 = Get-AtomicTechnique -Path ..\..\atomics\T1117\T1117.yaml
 .EXAMPLE
-Get the Atomic Tests For A Given Technique
-Get-AtomicTest $T1117
-
+Generate the Atomic Tests For A Given Technique, don't execute.
+Invoke-AtomicTest $T1117 -GenerateOnly
+.EXAMPLE
+Execute the Atomic Tests For A Given Technique
+$T1117 = Get-AtomicTechnique -Path ..\..\atomics\T1117\T1117.yaml
+Invoke-AtomicTest $T1117
 .NOTES
 This script converts Atomic Tests Expressed in YAML into PowerShell Objects.
 
@@ -34,127 +36,94 @@ function Get-AtomicTechnique {
 [CmdletBinding()]
 Param(
 
-	[string]
-	$Path
+    [string]
+    $Path
 )
 # Returns A HashTable For Each File Passed In
 BEGIN { }
 PROCESS {
-		foreach ($File in $Path)
-		{
-			$parsedYaml = (ConvertFrom-Yaml (Get-Content $File -Raw ))
-			Write-Output $parsedYaml
-		}
+        foreach ($File in $Path)
+        {
+            $parsedYaml = (ConvertFrom-Yaml (Get-Content $File -Raw ))
+            Write-Output $parsedYaml
+        }
 }
 END { }
 
 }
 
-function Get-AtomicTest{
-[CmdletBinding()]
-Param(
-	[System.Collections.Hashtable]
-	$AtomicTechnique
-)
-BEGIN{}
-PROCESS {
-	foreach ($Technique in $AtomicTechnique)
-	{
-
-		$AtomicTest = $Technique.atomic_tests
-
-		foreach ($Test in $AtomicTest)
-		{
-			#Only Process Windows Tests For Now
-			if ( !($Test.supported_platforms.Contains('windows')) ){ return }
-
-			#Reject Manual Tests
-			if ( ($Test.executor.name.Contains('manual')) ) 	{ return }
-			Write-Host "[********BEGIN TEST*******]`n" $Technique.display_name.ToString(), $Technique.attack_technique.ToString() "has" $Technique.atomic_tests.Count "Test(s)"  -Foreground Yellow
-			Write-Host $Test.name.ToString()
-			Write-Host $Test.description.ToString()
-
-			$finalCommand = $Test.executor.command
-			if($Test.input_arguments.Count -gt 0)
-			{
-				$InputArgs = [Array]($Test.input_arguments.Keys).Split(" ")
-				$InputDefaults = [Array]( $Test.input_arguments.Values | %{$_.default }).Split(" ")
-
-				for($i = 0; $i -lt $InputArgs.Length; $i++)
-				{
-					$findValue = '#{' + $InputArgs[$i] + '}'
-					$finalCommand = $finalCommand.Replace( $findValue, $InputDefaults[$i] )
-				}
-				Write-Output $finalCommand
-			}
-			else
-			{
-				Write-Output $finalCommand
-			}
-
-		}
-
-		Write-Host "[!!!!!!!!END TEST!!!!!!!]`n"
-
-
-	}
-}
-END {}
-
-}
-
-
 function Invoke-AtomicTest{
 [CmdletBinding()]
 Param(
-	[System.Collections.Hashtable]
-	$AtomicTechnique
+    [System.Collections.Hashtable]
+    $AtomicTechnique,
+
+    [switch]
+    $GenerateOnly
 )
 BEGIN {}
 PROCESS {
-	foreach ($Technique in $AtomicTechnique)
-		{
+    foreach ($Technique in $AtomicTechnique)
+        {
 
-			$AtomicTest = $Technique.atomic_tests
+            $AtomicTest = $Technique.atomic_tests
 
-			foreach ($Test in $AtomicTest)
-			{
-				#Only Process Windows Tests For Now
-				if ( !($Test.supported_platforms.Contains('windows')) ){ return }
+            foreach ($Test in $AtomicTest)
+            {
+                #Only Process Windows Tests For Now
+                if(!($Test.supported_platforms.Contains('windows')) ){
+                    return
+                }
+                #Reject Manual Tests
+                if ( ($Test.executor.name.Contains('manual')) ){
+                    return
+                }
+                Write-Host ("[********EXECUTING TEST*******]`n" +
+                $Technique.display_name.ToString(), $Technique.attack_technique.ToString() )
+                Write-Host $Test.name.ToString()
+                Write-Host $Test.description.ToString()
 
-				#Reject Manual Tests
-				if ( ($Test.executor.name.Contains('manual')) ) 	{ return }
-				Write-Host "[********EXECUTING TEST*******]`n" $Technique.display_name.ToString(), $Technique.attack_technique.ToString()  -Foreground Yellow
-				Write-Host $Test.name.ToString()
-				Write-Host $Test.description.ToString()
+                $finalCommand = $Test.executor.command
+                if($Test.input_arguments.Count -gt 0)
+                {
+                    #Replace InputArgs with default values
+                    $InputArgs = [Array]($Test.input_arguments.Keys).Split(" ")
+                    $InputDefaults = [Array]( $Test.input_arguments.Values | %{$_.default }).Split(" ")
 
-				$finalCommand = $Test.executor.command
-				if($Test.input_arguments.Count -gt 0)
-				{
-					#Fix up, Replace InputArgs
-					$InputArgs = [Array]($Test.input_arguments.Keys).Split(" ")
-					$InputDefaults = [Array]( $Test.input_arguments.Values | %{$_.default }).Split(" ")
+                    for($i = 0; $i -lt $InputArgs.Length; $i++)
+                    {
+                        $findValue = '#{' + $InputArgs[$i] + '}'
+                        $finalCommand = $finalCommand.Replace( $findValue, $InputDefaults[$i] )
+                    }
 
-					for($i = 0; $i -lt $InputArgs.Length; $i++)
-					{
-						$findValue = '#{' + $InputArgs[$i] + '}'
-						$finalCommand = $finalCommand.Replace( $findValue, $InputDefaults[$i] )
-					}
+                }
 
-				}
+                #Get Executor and Build Command Script
+                if($GenerateOnly)
+                {
+                    Write-Host $finalCommand -Foreground Green
+                }
+                else
+                {
+                    switch ($Test.executor.name) {
 
-				#Get Executor and Build Command Script
-				switch ($Test.executor.name) {
+                    "command_prompt" {
+                        Write-Host "Command Prompt:`n $finalCommand"  -Foreground Green;
+                        $execCommand = $finalCommand.Split("`n");
+                        $execCommand | %{ iex "cmd.exe /c $_" }
+                        break; }
+                    "powershell" {
+                        Write-Host "PowerShell`n $finalCommand" -Foreground Cyan;
+                        $execCommand = "Invoke-Command -ScriptBlock {$finalCommand}";
+                        iex $execCommand;
+                        break }
+                    default {"Something horrible happened"; break}
+                    }
+                }
+            }
 
-				"command_prompt" { Write-Host "Command Prompt`n $finalCommand"  -Foreground Green; break }
-				"powershell" { Write-Host "PowerShell`n $finalCommand" -Foreground Cyan; break }
-				default {"Something else happened"; break}
-				}
-
-			}
-
-			Write-Host "[!!!!!!!!END TEST!!!!!!!]`n`n" -Foreground Yellow
-		}
+            Write-Host "[!!!!!!!!END TEST!!!!!!!]`n`n" -Foreground Yellow
+        }
 
 }
 END {}
--- execution-frameworks/Invoke-AtomicRedTeam/README.md
+++ execution-frameworks/Invoke-AtomicRedTeam/README.md
@@ -8,21 +8,16 @@ Basic usage Examples:
     `C:\AtomicRedTeam\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam.ps1`
 - Execute Single Test.
 
-   `T1117 = Get-AtomicTechnique -Path ..\..\atomics\T1117\T1117.yaml`  
-   `Get-AtomicTest $T1117`  
+   `$T1117 = Get-AtomicTechnique -Path ..\..\atomics\T1117\T1117.yaml`  
+   `Invoke-AtomicTest $T1117`  
 
-- Fetch All Tests From All Yaml FileSystem
+- Execute All Tests
 
-    `[System.Collections.HashTable]$AllAtomicTests = @{};`  
-    `$AtomicFilePath = 'C:\AtomicRedTeam\atomics\';`  
-
-    `Get-Childitem $AtomicFilePath -Recurse -Filter \*.yaml -File | ForEach-Object {`  
-    `$currentTechnique = [System.IO.Path]::GetFileNameWithoutExtension($\_.FullName);`  
-    `$parsedYaml = (ConvertFrom-Yaml (Get-Content $\_.FullName -Raw ));`  
-    `$AllAtomicTests.Add($currentTechnique, $parsedYaml);`  
-    `New-Variable -Name $currentTechnique -Value $parsedYaml -Scope "Local";`  
-    `}`  
-    `$AllAtomicTests.T1117 | %{ Get-AtomicTest $_ }`
-
-
+    `[System.Collections.HashTable]$AllAtomicTests = @{};`
+    `$AtomicFilePath = 'C:\AtomicRedTeam\atomics\';`
+    `Get-Childitem $AtomicFilePath -Recurse -Filter *.yaml -File | ForEach-Object {`
+    `$currentTechnique = [System.IO.Path]::GetFileNameWithoutExtension($_.FullName);`
+    `$parsedYaml = (ConvertFrom-Yaml (Get-Content $_.FullName -Raw ));`
+    `$AllAtomicTests.Add($currentTechnique, $parsedYaml);`
+    
 - Feedback Welcome
