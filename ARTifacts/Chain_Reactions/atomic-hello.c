4608e8f00f391567a190cf8625c755c57907c6fc
ForensicITGuy <tmlambert13@gmail.com>
2019-01-23 14:32:58 -0600

    initial commit

---
+++ ARTifacts/Chain_Reactions/atomic-hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+ 
+
+//  Simple Hello World for Atomic Red Team payload
+
+int main() { 
+  
+           printf("Hello from Atomic Red Team! \n"); 
+               
+           return 0; 
+}
