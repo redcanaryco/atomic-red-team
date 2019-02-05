1a185fe7f7370df8015385a5fae3aab8bc12e9ec
Michael Haag <mike@redcanary.com  git config --global user.name Michael Haag>
2019-02-05 16:59:48 -0700

    Update install-atomicredteam.ps1

--- execution-frameworks/Invoke-AtomicRedTeam/install-atomicredteam.ps1
+++ execution-frameworks/Invoke-AtomicRedTeam/install-atomicredteam.ps1
@@ -1,4 +1,14 @@
 #Requires -RunAsAdministrator
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$False,Position=0)]
+  [string]$InstallPath = 'C:\AtomicRedTeam',
+
+  [Parameter(Mandatory=$False,Position=0)]
+  [string]$DownloadPath = 'C:\AtomicRedTeam'
+
+ )
+
 
 function Install-AtomicRedTeam {
 <#
@@ -73,37 +83,46 @@ function Install-AtomicRedTeam {
 
 write-verbose "Directory Creation"
 
-mkdir c:\AtomicRedTeam\
+if(!(Test-Path -Path $InstallPath )){
+    New-Item -ItemType directory -Path $InstallPath
+	write-verbose "Setting Execution Policy to Unrestricted"
+	set-executionpolicy Unrestricted
 
-write-verbose "Setting Execution Policy to Unrestricted"
-set-executionpolicy Unrestricted
+	write-verbose "Setting variables for remote URL and download Path"
+	$url = "https://github.com/redcanaryco/atomic-red-team/archive/master.zip"
+	$path = "$DownloadPath\master.zip"
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	$webClient = new-object System.Net.WebClient
+	write-verbose "Beginning download from Github"
+	$webClient.DownloadFile( $url, $path )
 
-write-verbose "Setting variables for remote URL and download Path"
-$url = "https://github.com/redcanaryco/atomic-red-team/archive/master.zip"
-$path = "C:\AtomicRedTeam\master.zip"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$webClient = new-object System.Net.WebClient
-write-verbose "Beginning download from Github"
-$webClient.DownloadFile( $url, $path )
+	write-verbose "Extracting ART to C:\AtomicRedTeam\"
+	expand-archive -LiteralPath "$DownloadPath\master.zip" -DestinationPath "$InstallPath"
 
-write-verbose "Extracting ART to C:\AtomicRedTeam\"
-expand-archive -LiteralPath c:\atomicRedTeam\master.zip -DestinationPath C:\AtomicRedTeam\
+	write-verbose "Installing NuGet PackageProvider"
+	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
-write-verbose "Installing NuGet PackageProvider"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+	write-verbose "Installing powershell-yaml"
+	Install-Module -Name powershell-yaml -Force
 
-write-verbose "Installing powershell-yaml"
-Install-Module -Name powershell-yaml
+	write-verbose "Importing invoke-atomicRedTeam module"
+	Import-Module "$InstallPath\atomic-red-team-master\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam.psm1"
 
-write-verbose "Importing invoke-atomicRedTeam module"
-Import-Module C:\AtomicRedTeam\atomic-red-team-master\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam.psm1
+	write-verbose "Changing current work directory Invoke-AtomicRedTeam"
+	cd "$InstallPath\atomic-red-team-master\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam\"
 
-write-verbose "Changing current work directory Invoke-AtomicRedTeam"
-cd C:\AtomicRedTeam\atomic-red-team-master\execution-frameworks\Invoke-AtomicRedTeam\Invoke-AtomicRedTeam\
+	write-verbose "Clearing screen"
+	clear
 
-write-verbose "Clearing screen"
-clear
+	Write-Host "Installation of Invoke-AtomicRedTeam is complete" -Fore Yellow
 
-Write-Host "Installation of Invoke-AtomicRedTeam is complete" -Fore Yellow
+}
+else
+{
+	Write-Verbose "Atomic Already exists at $InstallPath"
+	exit
+
+
+}
 }
 Install-AtomicRedTeam
