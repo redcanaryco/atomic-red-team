0dcde71a15a7bfa2af9884ced2db40a4bebd870c
Carrie Roberts <clr2of8@gmail.com>
2020-01-22 20:36:20 -0700

    Asynchronous Attack Execution and other handy things (#790)
    * execute attack in separate process
    
    * install from custom repoOwner and branch
    
    * remove zip after install
    
    * added showdetails brief and sleep for linux output
    
    * remove positional param spec
    
    * replacing special PathToAtomicsFolder in commands
    
    * use pwsh on linux
    
    * kill proc tree linux
    
    * include path in remove-item
    
    * update readme
    
    * update readme
    
    * update readme
    
    Co-authored-by: Tony M Lambert <ForensicITGuy@users.noreply.github.com>

--- execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam/Private/Invoke-CheckPrereqs.ps1
+++ execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam/Private/Invoke-CheckPrereqs.ps1
@@ -1,4 +1,4 @@
-function Invoke-CheckPrereqs ($test, $isElevated, $customInputArgs, $PathToAtomicsFolder) {
+function Invoke-CheckPrereqs ($test, $isElevated, $customInputArgs, $PathToAtomicsFolder, $TimeoutSeconds) {
     $FailureReasons = New-Object System.Collections.ArrayList
     if ($test.executor.elevation_required -and -not $isElevated) {
         $FailureReasons.add("Elevation required but not provided`n") | Out-Null
@@ -6,9 +6,10 @@ function Invoke-CheckPrereqs ($test, $isElevated, $customInputArgs, $PathToAtomi
     foreach ($dep in $test.dependencies) {
         $executor = Get-PrereqExecutor $test
         $final_command = Merge-InputArgs $dep.prereq_command $test $customInputArgs $PathToAtomicsFolder
-        $success = Invoke-ExecuteCommand $final_command $executor
+        $final_command = ($final_Command.trim()).Replace("`n", " && ")
+        $res = Invoke-ExecuteCommand $final_command $executor $TimeoutSeconds
         $description = Merge-InputArgs $dep.description $test $customInputArgs $PathToAtomicsFolder
-        if (-not $success) {
+        if ($res -ne 0) {
             $FailureReasons.add($description) | Out-Null
         }
     }
