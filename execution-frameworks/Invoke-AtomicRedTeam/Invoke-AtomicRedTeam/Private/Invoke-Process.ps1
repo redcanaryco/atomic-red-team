883bc38604c4a9a87af84939f2e49723d61e1f06
clr2of8 <clr2of8@gmail.com>
2020-02-04 14:05:40 -0700

    default working dir is tmp

--- execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam/Private/Invoke-Process.ps1
+++ execution-frameworks/Invoke-AtomicRedTeam/Invoke-AtomicRedTeam/Private/Invoke-Process.ps1
@@ -11,7 +11,7 @@ function Invoke-Process {
         [string]$Arguments = "",
         
         [Parameter(Mandatory = $false, Position = 2)]
-        [string]$WorkingDirectory = "$env:TEMP",
+        [string]$WorkingDirectory = $( if ($IsLinux -or $IsMacOS) {"/tmp" } else { $env:TEMP }),
 
         [Parameter(Mandatory = $false, Position = 3)]
         [Int]$TimeoutSeconds = 120
