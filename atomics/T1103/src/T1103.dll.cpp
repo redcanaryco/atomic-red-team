65b888808a70cc9b1e3d38f477f8d5521183326b
clr2of8 <clr2of8@gmail.com>
2020-04-07 12:05:47 -0600

    test works out of the box now

---
+++ atomics/T1103/src/T1103.dll.cpp
@@ -0,0 +1,20 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+	)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(0, L"Install AppInit Shim DLL was called!", 0, 0);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
