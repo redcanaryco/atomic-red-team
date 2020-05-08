7c593943e27fc9f157c1f0b926a9e7aedc9eede6
clr2of8 <clr2of8@gmail.com>
2020-05-08 00:26:25 -0600

    debugging

--- bin/generate-guids.rb
+++ bin/generate-guids.rb
@@ -1,29 +1,29 @@
 #! /usr/bin/env ruby
-# $LOAD_PATH << "#{File.dirname(File.dirname(__FILE__))}/atomic_red_team" unless $LOAD_PATH.include? "#{File.dirname(File.dirname(__FILE__))}/atomic_red_team"
-# require 'yaml'
-# require 'atomic_red_team'
+$LOAD_PATH << "#{File.dirname(File.dirname(__FILE__))}/atomic_red_team" unless $LOAD_PATH.include? "#{File.dirname(File.dirname(__FILE__))}/atomic_red_team"
+require 'yaml'
+require 'atomic_red_team'
 
-# ATOMIC_RED_TEAM = AtomicRedTeam.new
-# USED_GUIDS_FILE = "#{File.dirname(File.dirname(__FILE__))}/atomics/used_guids.txt"
+ATOMIC_RED_TEAM = AtomicRedTeam.new
+USED_GUIDS_FILE = "#{File.dirname(File.dirname(__FILE__))}/atomics/used_guids.txt"
 
-# oks = []
-# fails = []
+oks = []
+fails = []
 
-# ATOMIC_RED_TEAM.atomic_test_paths.each do |path|
-#   begin
-#     print "Generating guids #{path}..."
-#     YAML.load_file(path)
-#     AtomicRedTeam.new.generate_guids_for_yaml!(path, USED_GUIDS_FILE)
+ATOMIC_RED_TEAM.atomic_test_paths.each do |path|
+  begin
+    print "Generating guids #{path}..."
+    YAML.load_file(path)
+    AtomicRedTeam.new.generate_guids_for_yaml!(path, USED_GUIDS_FILE)
 
-#     oks << path
-#     puts "OK"
-#   rescue => ex
-#     fails << path
-#     puts "FAIL\n#{ex}\n"
-#   end
-# end
+    oks << path
+    puts "OK"
+  rescue => ex
+    fails << path
+    puts "FAIL\n#{ex}\n"
+  end
+end
 
-# puts
-# puts "#{oks.count + fails.count} techniques, #{fails.count} failures"
+puts
+puts "#{oks.count + fails.count} techniques, #{fails.count} failures"
 
-# exit fails.count
+exit fails.count
