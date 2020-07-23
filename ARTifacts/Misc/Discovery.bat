36f83b728bc26a49eacb0535edc42be8c377ac54
Sergio Gonzalez <58481372+rc-sgonzalez@users.noreply.github.com>
2020-07-23 11:57:55 -0400

    Update Discovery.bat (#1154)

--- ARTifacts/Misc/Discovery.bat
+++ ARTifacts/Misc/Discovery.bat
@@ -8,7 +8,7 @@ net config workstation
 net accounts
 net accounts /domain
 net view
-sc query
+sc.exe query
 reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows"
 reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce
 reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce
