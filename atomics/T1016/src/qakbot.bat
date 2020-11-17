0b844ac0bdaa61d21d2e2688ac4f1a288f3abc0e
Brian Thacker <neillthacker101@gmail.com>
2020-11-16 22:40:33 -0600

    Create qakbot.bat

---
+++ atomics/T1016/src/qakbot.bat
@@ -0,0 +1,10 @@
+whoami /all
+cmd /c set
+arp -a
+ipconfig /all
+net view /all
+nslookup -querytype=ALL -timeout=10 _ldap._tcp.dc._msdcs.WORKGROUP
+net share
+route print
+netstat -nao
+net localgroup
