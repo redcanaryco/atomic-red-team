fd47161006eee73b96372ad488b2a58bd06c923e
Bhavin Patel <bhavin.j.patel91@gmail.com>
2021-06-24 15:44:35 -0500

    AWS Cloud atomics (#1457)
    * cloud atomics
    
    * adding new platform names
    
    * text and variable updates
    
    * minor
    
    * Update T1136.003.yaml
    
    Co-authored-by: bpatel <bpatel@splunk.com>
    Co-authored-by: Jose Enrique Hernandez <josehelps@gmail.com>
    Co-authored-by: Michael Haag <5632822+MHaggis@users.noreply.github.com>

---
+++ atomics/T1098.001/bin/aws_secret.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+echo "Creating Profile in ./aws/credentials"
+access_key=`cat aws_secret.creds| jq -r '.AccessKey.AccessKeyId'`
+secret_key=`cat aws_secret.creds| jq -r '.AccessKey.SecretAccessKey'`
+line=`grep -n atomicredteam ~/.aws/credentials | cut -d : -f1 |bc` 
+access="$(($line+1))"
+secret="$(($line+2))"
+sed -i '' "${access}s|aws_access_key_id = .*$|aws_access_key_id = $access_key|g" ~/.aws/credentials
+sed -i '' "${secret}s|aws_secret_access_key = .*$|aws_secret_access_key = $secret_key|g" ~/.aws/credentials
+	
+
