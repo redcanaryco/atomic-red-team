c6442f7526f84820ac12f5a3a3934f39dcc98c1d
Kelsey Seymour <99087145+KelseySeymour@users.noreply.github.com>
2022-02-05 21:10:54 -0500

    Addition of T1546.015
    Details:
    Creation of T1546.015 - COM Hijacking
    
    Testing:
    W10 Build 19044.1466
    
    Associated Issues:
    None found

---
+++ atomics/T1546.015/src/AtomicTest.cpp
@@ -0,0 +1,20 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(0, L"Atomic Test T1546.015", 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+
+}
