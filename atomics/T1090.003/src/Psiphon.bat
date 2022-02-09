cb8e54ba92e30cf10dda0df5d6630463505b2ef6
Chase James <cjames@nti.local>
2022-02-08 23:31:13 -0600

    message

---
+++ atomics/T1090.003/src/Psiphon.bat
@@ -0,0 +1,6 @@
+@echo off
+start C:\Users\%USERNAME%\Downloads\psiphon3.exe
+timeout /t 20 >nul 2>&1
+Taskkill /IM msedge.exe /F >nul 2>&1
+Taskkill /IM psiphon3.exe /F >nul 2>&1
+Taskkill /IM psiphon-tunnel-core.exe /F >nul 2>&1
