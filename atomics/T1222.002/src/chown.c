9307edbf71eed46dba7d44b6eecc6b5fd61c53d9
Brendan Malone <46428809+RoundBunny@users.noreply.github.com>
2022-06-30 13:39:29 -0500

    Update chown.c

--- atomics/T1222.002/src/chown.c
+++ atomics/T1222.002/src/chown.c
@@ -1,21 +1,14 @@
-//
-//  main.c
-//  T1222.002own
-//
-//  Created by bam on 6/27/22.
-//
-
 #include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     if (argc < 2) {
       printf("usage: %s </path/to/file>\n", argv[0]);
       return 2;
     }
 
+    //change user and group to root
     const char *filepath = argv[1];
     int rv = chown(filepath, 0, 0);
     if (0 != rv) {
