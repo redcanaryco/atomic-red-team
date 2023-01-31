c146a1b672fb5ed19045a5de867d9a8791a63915
MSAdministrator <MSAdministrator@users.noreply.github.com>
2023-01-31 16:47:27 -0600

    fix: Adding an updated/better version of the python validation but may ultimately be removed

--- bin/validate/validate.py
+++ bin/validate/validate.py
@@ -4,6 +4,7 @@ import json
 
 import yaml
 from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 
 schema = json.load(open("./bin/validate/atomic-red-team.schema.json"))
@@ -16,5 +17,13 @@ for item in glob.glob("./atomics/T*/T*.yaml"):
             instance=data,
             schema=schema
         )
+    except ValidationError as ve:
+        print(f"Error occurred with {item}.")
+        print("Each of the following are why it failed:")
+        print(f"\n\t{ve.context[0].message}\n")
+        print(f"The JSON Path is {ve.json_path}")
     except Exception as e:
-        print(item, e)
+        print(f"Error occurred with {item}.")
+        print("Each of the following are why it failed:")
+        print(f"\n\t{ve.context[0].message}\n")
+        print(f"The JSON Path is {ve.json_path}")
