4946dbbc59e1dd9267733bb2370c14608491bd2f
MSAdministrator <MSAdministrator@users.noreply.github.com>
2023-02-01 11:31:50 -0600

    fix: Updated validate.rb to verify that the Technique IDs are in the correct format.

--- bin/validate/validate.rb
+++ bin/validate/validate.rb
@@ -3,8 +3,16 @@ require "json"
 require "json-schema"
 
 
-schema_file = File.open("./bin/validate/atomic-red-team.schema.json").read
+schema_file = File.open("./bin/validate/atomic-red-team.schema.yaml").read
 schema = YAML.load(schema_file)
+
+# Validating that the attack_technique property is in the correct format. This is based off of the `format: technique_id` attribute in the schema.
+
+format_proc = -> value {
+  raise JSON::Schema::CustomFormatError.new("Must be T1234 format.") unless value.match("T#{/[0-9]/}") or value.match("T#{/[0-9]/}\./[0-9]/")
+}
+# register the proc for format 'technique_id' for schema
+JSON::Validator.register_format_validator("technique_id", format_proc)
 
 Dir.glob("./atomics/T*/T*.yaml").each do |atomic_test|
     puts "Attempting to validate Atomic Test #{atomic_test}."
@@ -13,9 +21,11 @@ Dir.glob("./atomics/T*/T*.yaml").each do |atomic_test|
     begin
         JSON::Validator.validate!(schema, a_test)
     rescue JSON::Schema::ValidationError => e
-        puts e.message
+        puts "Error of type '#{e.class}' occurred."
+        puts "\n#{e.message}"
     rescue JSON::Schema::JsonParseError => e
         puts e
     end
+    JSON::Validator::fully_validate(schema, a_test, :errors_as_objects => true)
     puts "Successfully validated Atomic Test #{atomic_test}."
 end
