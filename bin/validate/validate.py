25e1862bc1aa4258cbf6ae43ba7b3bc53536cfa1
Hare Sudhan <code@0x6c.dev>
2023-05-12 17:04:42 -0400

    github actions fix

--- bin/validate/validate.py
+++ bin/validate/validate.py
@@ -1,11 +1,12 @@
 """Validates atomics based on JSON Schema."""
 import glob
 import os.path
-
+import sys
 import yaml
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
+is_exception = False
 with open(f"{os.path.dirname(os.path.abspath(__file__))}/atomic-red-team.schema.yaml", "r") as f:
     schema = yaml.safe_load(f)
 
@@ -25,7 +26,12 @@ with open(f"{os.path.dirname(os.path.abspath(__file__))}/atomic-red-team.schema.
             else:
                 print(f"\n\t{ve}\n")
             print(f"The JSON Path is {ve.json_path}")
+            is_exception = True
         except Exception as e:
             print(f"Error occurred with {item}")
             print("Each of the following are why it failed:")
             print(f"\n\t{e}\n")
+            is_exception = True
+
+if is_exception:
+    sys.exit(1)
