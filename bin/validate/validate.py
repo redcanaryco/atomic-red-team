81cccd5018a9b539dc8b111716277ad1dd3f9fa7
Hare Sudhan <code@0x6c.dev>
2023-05-12 14:53:48 -0400

    minor schema changes

--- bin/validate/validate.py
+++ bin/validate/validate.py
@@ -1,29 +1,31 @@
 """Validates atomics based on JSON Schema."""
 import glob
-import json
+import os.path
 
 import yaml
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
+with open(f"{os.path.dirname(os.path.abspath(__file__))}/atomic-red-team.schema.yaml", "r") as f:
+    schema = yaml.safe_load(f)
 
-schema = json.load(open("./bin/validate/atomic-red-team.schema.json"))
-
-for item in glob.glob("./atomics/T*/T*.yaml"):
-    with open(item, 'r') as file:
-        data = yaml.safe_load(file)
-    try:
-        response = validate(
-            instance=data,
-            schema=schema
-        )
-    except ValidationError as ve:
-        print(f"Error occurred with {item}.")
-        print("Each of the following are why it failed:")
-        print(f"\n\t{ve.context[0].message}\n")
-        print(f"The JSON Path is {ve.json_path}")
-    except Exception as e:
-        print(f"Error occurred with {item}.")
-        print("Each of the following are why it failed:")
-        print(f"\n\t{ve.context[0].message}\n")
-        print(f"The JSON Path is {ve.json_path}")
+    for item in glob.glob("./atomics/T*/T*.yaml"):
+        with open(item, 'r') as file:
+            data = yaml.safe_load(file)
+        try:
+            validate(
+                instance=data,
+                schema=schema
+            )
+        except ValidationError as ve:
+            print(f"Error occurred with {item}")
+            print("Each of the following are why it failed:")
+            if (context := ve.context) and len(context) > 0:
+                print(f"\n\t{context[0].message}\n")
+            else:
+                print(f"\n\t{ve}\n")
+            print(f"The JSON Path is {ve.json_path}")
+        except Exception as e:
+            print(f"Error occurred with {item}")
+            print("Each of the following are why it failed:")
+            print(f"\n\t{e}\n")
