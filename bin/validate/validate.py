55b0a49b8268b6307fa330179dbac64660b90578
Hare Sudhan <code@0x6c.dev>
2023-05-14 00:47:38 -0400

    more validations added

--- bin/validate/validate.py
+++ bin/validate/validate.py
@@ -1,40 +1,130 @@
-"""Validates atomics based on JSON Schema."""
+import fnmatch
 import glob
-import os.path
-import sys
+import os
+from os import DirEntry
 import yaml
-from jsonschema import validate
-from jsonschema.exceptions import ValidationError
+import sys
+from jsonschema import validate, ValidationError
+from collections import defaultdict
 
-is_exception = False
-with open(f"{os.path.dirname(os.path.abspath(__file__))}/atomic-red-team.schema.yaml", "r") as f:
-    schema = yaml.safe_load(f)
 
-    for item in glob.glob("./atomics/T*/T*.yaml"):
-        with open(item, 'r') as file:
-            data = yaml.safe_load(file)
-        try:
-            validate(
-                instance=data,
-                schema=schema
-            )
-        except ValidationError as ve:
-            print(f"Error occurred with {item}")
+class BaseError(Exception):
+    def __init__(self, path):
+        self.path = path
+
+
+class InvalidPath(BaseError):
+    def __str__(self):
+        return f"Invalid path. `src` and `bin` are the only two directories supported."
+
+
+class InvalidFileName(BaseError):
+    def __str__(self):
+        return f"Invalid filename. Rename file from .yml to .yaml"
+
+
+class UnusedArgument(BaseError):
+    def __init__(self, path, argument, test_number):
+        super().__init__(path)
+        self.argument = argument
+        self.test_number = test_number
+
+    def __str__(self):
+        return f"Unused Input Argument {self.argument} for test number {self.test_number}"
+
+
+class Validator:
+    errors = defaultdict(list)
+
+    def __init__(self):
+        with open(f"{os.path.dirname(os.path.abspath(__file__))}/atomic-red-team.schema.yaml", "r") as f:
+            self.schema = yaml.safe_load(f)
+
+    def validate(self, obj: DirEntry):
+        if obj.is_file():
+            if fnmatch.fnmatch(obj.name, "*.yaml"):
+                self.validate_file(obj)
+        if obj.is_dir():
+            self.validate_directory(obj)
+
+    def validate_file(self, file: DirEntry):
+        """Performs file validation"""
+        self.validate_yaml_extension(file)
+        self.validate_input_args(file)
+        self.validate_json_schema(file)
+
+    def validate_input_args(self, file: DirEntry):
+        """Validates whether the defined input args are used."""
+        with open(file.path, "r") as f:
+            atomic = yaml.safe_load(f)
+            for index, t in enumerate(atomic["atomic_tests"]):
+                if args := t.get("input_arguments"):
+                    for k in list(args.keys()):
+                        variable = f"#{{{k}}}"
+                        executor = t.get("executor", {})
+                        deps = t.get("dependencies", [])
+
+                        commands = [executor.get("command"), executor.get("cleanup_command"), executor.get("steps")]
+                        commands += [d.get("get_prereq_command") for d in deps]
+                        commands += [d.get("prereq_command") for d in deps]
+                        commands = filter(lambda x: x is not None, commands)
+
+                        if not any([variable in c for c in commands]):
+                            self.errors[file.path].append(UnusedArgument(file.path, k, index + 1))
+
+    def validate_yaml_extension(self, file: DirEntry):
+        """Validates the yaml extension"""
+        if fnmatch.fnmatch(file.path, "*.yml"):
+            self.errors[file.path].append(InvalidFileName(file.path))
+
+    def validate_json_schema(self, file: DirEntry):
+        """Validates the yaml file against the schema."""
+        with open(file.path, "r") as f:
+            atomic = yaml.safe_load(f)
+            try:
+                validate(
+                    instance=atomic,
+                    schema=self.schema
+                )
+            except Exception as e:
+                self.errors[file.path].append(e)
+
+    def validate_directory(self, directory: DirEntry):
+        """Performs directory validation"""
+        self.validate_directory_path(directory)
+
+    def validate_directory_path(self, directory: DirEntry):
+        """Validated whether the directory is a allowed directory name (`src` or `bin`) """
+        if directory.name not in ["src", "bin"]:
+            self.errors[directory.path].append(InvalidPath(directory.path))
+
+    def print_errors(self):
+        """Output errors in a human friendly way."""
+        for i, errors in self.errors.items():
+            print(f"Error occurred with {i}.")
             print("Each of the following are why it failed:")
-            if (context := ve.context) and len(context) > 0:
-                print(f"\n\t{context[0].message}\n")
-            else:
-                print(f"\n\t{ve}\n")
-            print(f"The JSON Path is {ve.json_path}")
-            is_exception = True
-        except Exception as e:
-            print(f"Error occurred with {item}")
-            print("Each of the following are why it failed:")
-            print(f"\n\t{e}\n")
-            is_exception = True
+            for error in errors:
+                if isinstance(error, BaseError):
+                    print(f"\n\t{error}\n")
+                elif isinstance(error, ValidationError):
+                    if (context := error.context) and len(context) > 0:
+                        print(f"\n\t{context[0].message}\n")
+                    else:
+                        print(f"\n\t{error}\n")
+                    print(f"The JSON Path is {error.json_path}")
+                else:
+                    print(f"\n\t{error}\n")
 
-if is_exception:
+
+validator = Validator()
+
+for folder in glob.glob('./atomics/T*'):
+    for item in os.scandir(folder):
+        validator.validate(item)
+
+if len(validator.errors) > 0:
     print("Validation Failed")
+    validator.print_errors()
     sys.exit(1)
 else:
     print("Validation Successful")
