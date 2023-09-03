6bde0ddbc68d9e54618f7c7672679b2611193d97
Thomas Meng <44269971+thomasxmeng@users.noreply.github.com>
2023-09-03 12:56:29 +0100

    Update build.bat

--- atomics/T1055/src/x64/RedVanity/build.bat
+++ atomics/T1055/src/x64/RedVanity/build.bat
@@ -1,1 +1,1 @@
-cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tp red_vanity.c /link /OUT:VanityApp.exe /SUBSYSTEM:CONSOLE
+cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tp redVanity.c /link /OUT:redVanity.exe /SUBSYSTEM:CONSOLE
