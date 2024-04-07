477efb7cab5cdc8c0ee384732fb8e44d64735e2b
Hare Sudhan <code@0x6c.dev>
2024-04-07 04:05:57 -0400

    minor refactor

--- atomic_red_team/guid.py
+++ atomic_red_team/guid.py
@@ -4,7 +4,7 @@ from typing import List
 
 from ruamel.yaml import YAML
 
-from files import used_guids_file
+from common import used_guids_file
 
 yaml = YAML(typ="safe")
 
--- atomic_red_team/runner.py
+++ atomic_red_team/runner.py
@@ -10,7 +10,7 @@ from typing import Annotated
 import typer
 from pydantic import ValidationError
 
-from files import used_guids_file, base_path
+from common import used_guids_file, atomics_path
 from guid import (
     generate_guids_for_yaml,
     get_unique_guid,
@@ -28,7 +28,7 @@ def generate_guids():
     with open(used_guids_file, "r") as file:
         used_guids = file.readlines()
 
-    for file in glob.glob(f"{base_path}/T*/T*.yaml"):
+    for file in glob.glob(f"{atomics_path}/T*/T*.yaml"):
         generate_guids_for_yaml(file, partial(get_unique_guid, guids=used_guids))
 
 
@@ -47,7 +47,7 @@ def generate_schemas():
 def generate_counter():
     """Generate atomic tests count svg"""
     test_count = 0
-    for file in glob.glob(f"{base_path}/T*/T*.yaml"):
+    for file in glob.glob(f"{atomics_path}/T*/T*.yaml"):
         with open(file, "r") as f:
             yaml_data = yaml.load(f)
             if yaml_data is not None and "atomic_tests" in yaml_data:
@@ -67,8 +67,8 @@ def generate_counter():
 
 @app.command()
 def generate_labels(
-        pull_request: Annotated[str, typer.Option("--pr")],
-        token: Annotated[str, typer.Option("--token")],
+    pull_request: Annotated[str, typer.Option("--pr")],
+    token: Annotated[str, typer.Option("--token")],
 ):
     """Generate labels for a pull request."""
     api = GithubAPI(token)
@@ -84,7 +84,7 @@ def validate():
     validator = Validator()
     errors = defaultdict(list)
 
-    for folder in glob.glob(f"{base_path}/atomics/T*"):
+    for folder in glob.glob(f"{atomics_path}/atomics/T*"):
         for item in os.scandir(folder):
             try:
                 validator.validate(item)
@@ -96,7 +96,7 @@ def validate():
     else:
         print("Validation failed")
         for i, errors in errors.items():
-            print(f"Error occurred with {i.replace(f'{base_path}/', '')}.")
+            print(f"Error occurred with {i.replace(f'{atomics_path}/', '')}.")
             print("Each of the following are why it failed:")
             for error in errors:
                 if isinstance(error, ValidationError):
