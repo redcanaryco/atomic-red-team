19dec86cbb6b7c15f7d9e2690719aaad9ee9be58
Always in the Cage <always.in.the.cage@gmail.com>
2026-04-24 10:54:08 +0330

    Create T1568.002.yaml (#3320)
    Co-authored-by: Bhavin Patel <bhavin.j.patel91@gmail.com>

---
+++ atomics/T1568.002/src/T1568.002.py
@@ -0,0 +1,22 @@
+import datetime
+import random
+import string
+import subprocess
+import time
+
+TLDs = ['.com', '.net', '.org', '.ru', '.biz']
+
+def generate_domain(seed):
+  random.seed(seed)
+  length = random.randint(10, 15)
+  name = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+  return name + random.choice(TLDs)
+
+if __name__ == "__main__":
+  today = datetime.date.today().strftime('%Y%m%d')
+  print('[*] DGA cycle seed:', today)
+  for i in range(10):
+    domain = generate_domain(today + str(i))
+    print('[+] Querying:', domain)
+    subprocess.run(['dig', '+short', domain], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    time.sleep(2)
